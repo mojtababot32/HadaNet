bba7897d188bf23838fe3932f34ff2f715f40d89
Mojtaba <34414738+mojtabahad32@users.noreply.github.com>
2017-12-10 14:50:54 +0330

    @HadaNet

---
+++ plugins/delall.lua
@@ -0,0 +1,60 @@
+-- Edit @Mojtaba 
+-- channel @HadaNet 
+local function history(extra, suc, result)
+  for i=1, #result do
+    delete_msg(result[i].id, ok_cb, false)
+if tonumber(i) > 99 then
+    delete_msg(result[i].id, ok_cb, false)
+else
+if tonumber(i) > 99 then
+    delete_msg(result[i].id, ok_cb, false)
+else
+if tonumber(i) > 99 then
+    delete_msg(result[i].id, ok_cb, false)
+else
+if tonumber(i) > 99 then
+    delete_msg(result[i].id, ok_cb, false)
+else
+if tonumber(i) > 99 then
+    delete_msg(result[i].id, ok_cb, false)
+else
+if tonumber(i) > 99 then
+    delete_msg(result[i].id, ok_cb, false)
+else
+if tonumber(i) > 99 then
+    delete_msg(result[i].id, ok_cb, false)
+  end
+end
+end
+end
+end
+end
+end
+end
+  if tonumber(extra.con) == #result then
+    send_msg(extra.chatid, '"'..#result..'"پیام مورد نظر با موفقیت پاک شد', ok_cb, false)
+  else
+    send_msg(extra.chatid, 'پیام مورد نظر با موفقیت پاک شد!', ok_cb, false)
+  end
+end
+local function run(msg, matches)
+  if matches[1] == 'clean all'  and is_owner(msg) then
+    if msg.to.type == 'channel' then
+      get_history(msg.to.peer_id, 500 + 1 , history , {chatid = msg.to.peer_id, con = 500})
+    else
+      return "Only in supergroups"
+    end
+  else
+    return ""
+  end
+end
+
+return {
+    patterns = {
+        '^(clean all)$'
+    },
+    run = run
+}
+
+-- Edit @Mojtaba 
+-- channel @HadaNet 
